fca218252b205a9cd12cd19c6a0c56a461c14a65
ejaysrngy <ejaysarangay@gmail.com>
2022-01-03 14:16:39 +0800

    change directory name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import IndivExpense from "./components/IndivExpensee";
+
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <IndivExpense />
     </div>
   );
 }
---
+++ src/components/IndivExpense.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function IndivExpense(){
+    return (
+        <h2> Expense Item </h2>
+    )
+}
+
+export default IndivExpense;

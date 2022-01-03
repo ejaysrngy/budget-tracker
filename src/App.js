7f78a9b4eb033b25f804d9705ec11b13369aa003
ejaysrngy <ejaysarangay@gmail.com>
2022-01-03 14:45:18 +0800

    add styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from "react";
-import IndivExpense from "./components/IndivExpensee";
+import IndivExpense from "./components/IndivExpense";
 
 
 function App() {
+  const data = {
+    id: 1,
+    title: "Keyb",
+    amount: 2000.00,
+    date: Date.now()
+  }
+
+  console.log(data.title)
   return (
     <div>
       <h2>Let's get started!</h2>
-      <IndivExpense />
+      <IndivExpense 
+        title={data.title}
+        amount={data.amount}
+        date={data.date}
+      />
+      <IndivExpense 
+        title={data.title}
+        amount={data.amount}
+        date={data.date}
+      />
+      <IndivExpense 
+        title={data.title}
+        amount={data.amount}
+        date={data.date}
+      />
+      <IndivExpense 
+        title={data.title}
+        amount={data.amount}
+        date={data.date}
+      />
     </div>
   );
 }
--- src/components/IndivExpense.jsx
+++ src/components/IndivExpense.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import "./IndivExpense.css";
 
-function IndivExpense(){
+function IndivExpense(props){
     return (
-        <h2> Expense Item </h2>
+        <div className="indiv-expense">
+            <h3> {props.date} </h3>
+            <h2> {props.title} </h2>
+            <div className="indiv-expense-desc">
+                <h6> Php {props.amount} </h6>
+            </div>
+        </div>
     )
 }
 
